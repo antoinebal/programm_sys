237edd700e3f78ec5629795abdbe9a1218abf38e
M. Balayssac Antoine <abalayss@insa-10220>
2018-02-28 16:20:35 +0100

    test de pthread_cancel

--- td1_q.c
+++ td1_q.c
@@ -15,8 +15,21 @@ void * courroux(void* arg)
 }
 
 
+void * colere(void* arg)
+{
+  for(int ind=0; ind < *(int*)arg; ind++)
+    {
+      printf("et ma colere\n");
+      sleep(1);
+    }
+  return 0;
+  //pthread_exit(11);
+}
+
+
 int main(void)
 {
+
   pthread_t tid;
   int N;
   int M;
@@ -28,11 +41,30 @@ int main(void)
       printf("coucou\n");
       sleep(1);
     }
+
+  pthread_cancel(tid);
+  printf("tid cancelé");
+
+  pthread_t tid2;
+  pthread_create(&tid2, NULL, colere,(void*)&N);
+   for (int j=0; j<M; j++)
+    {
+      printf("aurevoir\n");
+      sleep(1);
+    }
+
   return 0;
+
+
   /* int** receptacle;
   int receptacle2;
   receptacle2=pthread_join(tid, receptacle);
   printf("%d", **receptacle);*/
+
+
+
+ 
+  
   }
   
 
